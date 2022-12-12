3d7c14956ffb57de43cd1ffac56efe318d01ef5f
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-12-12 04:08:00 +0100

    refactor: changed global starline var line from 'M' to 'Starline'

--- lua/starline/init.lua
+++ lua/starline/init.lua
@@ -1,8 +1,7 @@
-M = { }
-
+Starline = { }
 local mod_mode = require("starline.modules.mode")
 
-M.refresh = function()
+Starline.refresh = function()
   return table.concat {
     "%#Statusline#",
     mod_mode.mode(),
@@ -12,5 +11,5 @@ end
 local starline_ag = vim.api.nvim_create_augroup("Starline", { clear = false })
 vim.api.nvim_create_autocmd({"WinEnter ", "BufEnter"}, {
   group = starline_ag, pattern = "*",
-  command = "setlocal statusline=%!v:lua.M.refresh()"
+  command = "setlocal statusline=%!v:lua.Starline.refresh()"
 })
