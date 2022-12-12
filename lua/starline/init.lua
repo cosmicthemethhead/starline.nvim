2b78a9978c7d2f70419cf10bc9e1a65d2fba8807
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-12-12 03:31:13 +0100

    feat: added basic status-line

--- lua/starline/init.lua
+++ lua/starline/init.lua
@@ -1,7 +1,14 @@
-local M = { }
+M = { }
 
-M.hw = function()
-  print("Hello, World!")
+M.refresh = function()
+  return table.concat {
+    "%#Statusline#",
+    " Hello, World!",
+  }
 end
 
-return M
+local starline_ag = vim.api.nvim_create_augroup("Starline", { clear = false })
+vim.api.nvim_create_autocmd({"WinEnter ", "BufEnter"}, {
+  group = starline_ag, pattern = "*",
+  command = "setlocal statusline=%!v:lua.M.refresh()"
+})
